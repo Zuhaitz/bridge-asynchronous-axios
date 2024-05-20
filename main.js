7cd1bbdd35d7a8801e5563f343c3e79f4217e5bf
Zuhaitz <zuhaitzmartinezjimenez@gmail.com>
2024-05-20 19:40:00 +0200

    Exercise 1 finished and extra started

--- main.js
+++ main.js
@@ -1,6 +1,40 @@
-console.log("ME");
+// Exercise 1 - Draw Users
+const usersList = document.querySelector("#usersList");
+const showUsersBtn = document.querySelector("#showUsersBtn");
+let users = [];
 
 axios
   .get("https://jsonplaceholder.typicode.com/users")
-  .then((res) => console.log(res.data))
+  .then((res) => {
+    users = res.data;
+  })
+  .then(showUsersBtn.addEventListener("click", showUsers))
+  .catch((error) => console.error(error));
+
+function showUsers() {
+  // console.log(users);
+  // console.log(users.map((user) => user.name));
+
+  usersList.innerHTML = "";
+  users.forEach((user) => {
+    let name = document.createElement("p");
+    name.innerHTML = user.name;
+    usersList.appendChild(name);
+  });
+}
+
+// Exercise 2 - Doge
+axios
+  .get("https://dog.ceo/api/breeds/list/all")
+  .then((res) => console.log(Object.keys(res.data.message)))
+  .catch((error) => console.error(error));
+
+axios
+  .get("https://dog.ceo/api/breed/affenpinscher/images/random")
+  .then((res) => console.log(res.data.message))
+  .catch((error) => console.error(error));
+
+axios
+  .get("https://dog.ceo/api/breed/affenpinscher/images")
+  .then((res) => console.log(res.data.message))
   .catch((error) => console.error(error));
